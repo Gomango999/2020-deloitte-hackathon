c8665efa0cd38118113779af3c6abd0f62be0d83
pahad2000 <43926244+pahad2000@users.noreply.github.com>
2020-06-12 20:07:47 +1000

    Update GP.py

--- src/GP.py
+++ src/GP.py
@@ -5,3 +5,20 @@ class GP:
     @property
     def name(self):
         return self._name
+    
+   '''
+   Bad convention
+   class GP:
+	"""docstring for ClassName"""
+	def __init__(self, name, email, num):
+		self.name = name
+		self.email = email
+		self.num = num
+		self.profile = Profile(name, gender, date, email, num)
+		self.patients = []
+		self.numpts = 0
+
+	def addPt(self, patient):
+		self.patients[self.numpts] = patient
+		self.numpts += 1
+'''
