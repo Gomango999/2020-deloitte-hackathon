a927c8fbbc458750559f3c35fe4471d16197a6cf
pahad2000 <43926244+pahad2000@users.noreply.github.com>
2020-06-12 20:12:48 +1000

    Update Patient.py

--- src/Patient.py
+++ src/Patient.py
@@ -1,19 +1,13 @@
 class Patient:
-    def __init__(self, name):
-        self._name = name
+    def __init__(self, profile, responses):
+        self.profile = profile
+	self.numresps = 0
+	self.responses = []
+	
+    def addResponse(self, response):
+	self.response[self.numresps] = response
+	self.numresps += 1
     
     @property
     def name(self):
-        return self._name
-'''
-Since it's not good convention yet
-class Patient:
-	def __init__(self, profile, responses):
-		self.profile = profile
-		self.numresps = 0
-		self.responses = []
-
-	def addResponse(self, response):
-		self.response[self.numresps] = response
-		self.numresps += 1
-'''
+        return self.profile.name
