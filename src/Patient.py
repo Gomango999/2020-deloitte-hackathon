e900c3f108a51ef848a3362da7576153d122000b
pahad2000 <43926244+pahad2000@users.noreply.github.com>
2020-06-12 20:24:04 +1000

    Update Patient.py

--- src/Patient.py
+++ src/Patient.py
@@ -1,12 +1,10 @@
 class Patient:
     def __init__(self, profile, responses):
         self._profile = profile
-	self._numresps = 0
 	self._responses = []
 	
     def addResponse(self, response):
-	self._response[self.numresps] = response
-	self._numresps += 1
+	self._response[self._responses.len()] = response
     
     @property
     def name(self):
