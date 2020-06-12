f4b8865ecb5afa1c0a9b67d0654ad2719b4f577f
Parviz Ahadzada <parviz.ahadzada@hotmail.com>
2020-06-12 18:56:17 +1000

    Profile.py, Response.py updated

---
+++ src/Profile.py
@@ -0,0 +1,13 @@
+class Profile: 
+	Questions = {'Family History' : None,
+				 'Prescriptions/Drug Use' : None,
+				 'Alcohol' : None,
+				 'Smoking' : None,
+				 }
+
+	def __init__(self, name, gender, date, email, num):
+        self.name = name 
+        self.gender = gender
+        self.date = date
+        self.num = num
+
---
+++ src/Response.py
@@ -0,0 +1,7 @@
+class Response:
+	def __init__(self, provider, user, date, worstsymp, symp):
+		self.provider = #provider() doctor constructor
+		self.user = user
+		self.date = date
+		self.worstsymp = worstsymp
+		self.symp = #symp() constructor
