ae8505a8f93721cedaa9491c67d8522724c750a3
pahad2000 <43926244+pahad2000@users.noreply.github.com>
2020-06-12 20:09:17 +1000

    Update Symptom.py

---
+++ src/Symptom.py
@@ -0,0 +1,10 @@
+class Symptom:
+ 	"""docstring for ClassName"""
+ 	def __init__(self, name):
+ 		self.name = name
+ 		self.questions = []
+ 		self.numqs = 0
+
+ 	def addQuestion(question):
+ 		self.questions[self.numqs] = question
+ 		self.numqs += 1 
